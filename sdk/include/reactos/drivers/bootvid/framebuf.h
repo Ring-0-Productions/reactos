aa9e8ec1c1e8d177b5bc46346f6299c3d3fa1083
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2023-07-12 16:46:19 +0200

    Supplemental stuff for the framebuffer device info structure. Moved some uefi support helpers in there.

--- sdk/include/reactos/drivers/bootvid/framebuf.h
+++ sdk/include/reactos/drivers/bootvid/framebuf.h
@@ -34,10 +34,12 @@ typedef struct _CM_FRAMEBUF_DEVICE_DATA
     ULONG ScreenWidth;
     ULONG ScreenHeight;
 
-    /* Number of pixel elements per video memory line */
+    /* Number of pixel elements per video memory line. Related to the
+     * number of bytes per scan-line ("Pitch", or "ScreenStride") via:
+     * Pitch = PixelsPerScanLine * BytesPerPixel */
     ULONG PixelsPerScanLine;
 
-    ULONG BitsPerPixel; // PixelDepth
+    ULONG BitsPerPixel; // == BytesPerPixel * 8 ("PixelDepth")
 
     // MEMMODEL MemoryModel; // Linear, banked, ...
 
@@ -46,19 +48,96 @@ typedef struct _CM_FRAMEBUF_DEVICE_DATA
      * A bit being set defines those used for the given color component,
      * such as Red, Green, Blue, or Reserved.
      */
-    struct
+    // UCHAR NumberRedBits;
+    // UCHAR NumberGreenBits;
+    // UCHAR NumberBlueBits;
+    // UCHAR NumberReservedBits;
+    struct /*FB_PIXEL_BITMASK*/
     {
-        // UCHAR NumberRedBits;
-        // UCHAR NumberGreenBits;
-        // UCHAR NumberBlueBits;
-        // UCHAR NumberReservedBits;
         ULONG RedMask;
         ULONG GreenMask;
         ULONG BlueMask;
         ULONG ReservedMask;
-    } PixelInformation;
+    } PixelInformation; // PixelMasks;
 
 } CM_FRAMEBUF_DEVICE_DATA, *PCM_FRAMEBUF_DEVICE_DATA;
+
+
+#ifdef EFI_UGA_DRAW_PROTOCOL_GUID // EFI 1.x
+
+/* NOTE: EFI UGA does not support any other format than 32-bit xRGB, and
+ * no direct access to the underlying hardware framebuffer is offered. */
+// C_ASSERT(sizeof(EFI_UGA_PIXEL) == sizeof(ULONG));
+
+#endif // EFI
+
+// UEFI support, see efi/GraphicsOutput.h
+#ifdef EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID // __GRAPHICS_OUTPUT_H__
+
+C_ASSERT(RTL_FIELD_SIZE(CM_FRAMEBUF_DEVICE_DATA, PixelInformation) == sizeof(EFI_PIXEL_BITMASK));
+// C_ASSERT(sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL) == sizeof(ULONG));
+
+/**
+ * @brief   Maps UEFI GOP pixel format to pixel masks.
+ * @see     EFI_PIXEL_BITMASK
+ **/
+static EFI_PIXEL_BITMASK EfiPixelMasks[] =
+{ /* Red,        Green,      Blue,       Reserved */
+    {0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000},   // PixelRedGreenBlueReserved8BitPerColor
+    {0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000},   // PixelBlueGreenRedReserved8BitPerColor
+    {0,          0,          0,          0}             // PixelBitMask, PixelBltOnly, ...
+};
+
+// TODO: this version of the struct is temporary
+// REACTOS_INTERNAL_BGCONTEXT
+typedef struct _ROSEFI_FRAMEBUFFER_DATA
+{
+    ULONG_PTR    BaseAddress;
+    ULONG        BufferSize;
+    UINT32       ScreenWidth;
+    UINT32       ScreenHeight;
+    UINT32       PixelsPerScanLine;
+    UINT32       PixelFormat;
+} ROSEFI_FRAMEBUFFER_DATA, *PROSEFI_FRAMEBUFFER_DATA;
+
+#endif // UEFI
+
+
+/**
+ * @brief
+ * Calculates the number of bits per pixel ("PixelDepth") for
+ * the given pixel format, given by the pixel color masks.
+ *
+ * @note
+ * The calculation is done by finding the highest bit set in
+ * the combined pixel color masks.
+ *
+ * @remark
+ * See UEFI Spec Rev.2.10 Section 12.9 "Graphics Output Protocol":
+ * example code "GetPixelElementSize()" function.
+ **/
+FORCEINLINE
+ULONG
+PixelBitmasksToBpp(
+    ULONG RedMask,
+    ULONG GreenMask,
+    ULONG BlueMask,
+    ULONG ReservedMask)
+{
+    ULONG CompoundMask = (RedMask | GreenMask | BlueMask | ReservedMask);
+#if 1
+    ULONG ret = 0;
+    return (_BitScanReverse(&ret, CompoundMask) ? ret : 0);
+#else
+    ULONG ret = 32; // (8 * sizeof(ULONG));
+    while ((CompoundMask & (1 << 31)) == 0)
+    {
+        ret--;
+        CompoundMask <<= 1;
+    }
+    return ret;
+#endif
+}
 
 #ifdef __cplusplus
 }
