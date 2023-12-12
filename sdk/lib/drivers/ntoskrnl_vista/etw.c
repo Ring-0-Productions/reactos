a67d718023749d23448f5bf57eadb12771f404c7
Adam Słaboń <asaillen@protonmail.com>
2023-12-12 15:36:11 +0100

    [NTOSKRNL] Stubs for Windows 10 1511 WDF

--- sdk/lib/drivers/ntoskrnl_vista/etw.c
+++ sdk/lib/drivers/ntoskrnl_vista/etw.c
@@ -36,6 +36,22 @@ EtwWrite(
     return STATUS_NOT_IMPLEMENTED;
 }
 
+_IRQL_requires_max_(HIGH_LEVEL)
+NTSTATUS
+NTKRNLVISTAAPI
+NTAPI
+EtwWriteTransfer(
+    _In_ REGHANDLE RegHandle,
+    _In_ PCEVENT_DESCRIPTOR EventDescriptor,
+    _In_opt_ LPCGUID ActivityId,
+    _In_opt_ LPCGUID RelatedActivityId,
+    _In_ ULONG UserDataCount,
+    _In_reads_opt_(UserDataCount) PEVENT_DATA_DESCRIPTOR UserData)
+{
+    UNIMPLEMENTED;
+    return STATUS_NOT_IMPLEMENTED;
+}
+
 _IRQL_requires_max_(PASSIVE_LEVEL)
 NTSTATUS
 NTKRNLVISTAAPI
