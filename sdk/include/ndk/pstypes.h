3f65710ede0787b0a52ec14edaa691bfa870e9af
Justin Miller <Justin.miller@reactos.org>
2024-03-16 23:22:39 -0700

    [SDK] Windows compatible Tls Types

--- sdk/include/ndk/pstypes.h
+++ sdk/include/ndk/pstypes.h
@@ -1698,6 +1698,42 @@ typedef struct _WIN32_CALLOUTS_FPNS
 #endif
 } WIN32_CALLOUTS_FPNS, *PWIN32_CALLOUTS_FPNS;
 
+/* Tls moment */
+typedef struct _THREAD_TLS_INFORMATION
+{
+	ULONG      Flags;
+
+	union
+	{
+		PVOID* TlsVector;
+		PVOID  TlsModulePointer;
+	};
+
+	HANDLE     ThreadId;
+} THREAD_TLS_INFORMATION, *PTHREAD_TLS_INFORMATION;
+
+typedef enum _PROCESS_TLS_INFORMATION_TYPE
+{
+	ProcessTlsReplaceIndex,
+	ProcessTlsReplaceVector,
+	MaxProcessTlsOperation
+} PROCESS_TLS_INFORMATION_TYPE, *PPROCESS_TLS_INFORMATION_TYPE;
+
+typedef struct _PROCESS_TLS_INFORMATION
+{
+	ULONG                  Reserved;
+	ULONG                  OperationType;
+	ULONG                  ThreadDataCount;
+
+	union
+	{
+		ULONG              TlsIndex;
+		ULONG              TlsVectorLength;
+	};
+
+	THREAD_TLS_INFORMATION ThreadData[ANYSIZE_ARRAY];
+} PROCESS_TLS_INFORMATION, *PPROCESS_TLS_INFORMATION;
+
 #endif // !NTOS_MODE_USER
 
 #ifdef __cplusplus
