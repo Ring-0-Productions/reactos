7e8173e4e7ce19bba6363aa56d76ffa9e90f1735
Justin Miller <Justin.miller@reactos.org>
2024-06-16 15:58:34 -0700

    [NTOS:LPC] Remove old legacy LPC logic

--- ntoskrnl/lpc/listen.c
+++ ntoskrnl/lpc/listen.c
@@ -1,10 +1,3 @@
-/*
- * PROJECT:         ReactOS Kernel
- * LICENSE:         GPL - See COPYING in the top level directory
- * FILE:            ntoskrnl/lpc/listen.c
- * PURPOSE:         Local Procedure Call: Listening
- * PROGRAMMERS:     Alex Ionescu (alex.ionescu@reactos.org)
- */
 
 /* INCLUDES ******************************************************************/
 
@@ -14,48 +7,16 @@
 
 /* PUBLIC FUNCTIONS **********************************************************/
 
-/*
- * @implemented
- */
 NTSTATUS
 NTAPI
-NtListenPort(IN HANDLE PortHandle,
-             OUT PPORT_MESSAGE ConnectMessage)
+NtListenPort(
+    _In_ HANDLE PortHandle,
+    _In_ PPORT_MESSAGE ConnectionRequest)
 {
-    NTSTATUS Status;
-
     PAGED_CODE();
-    LPCTRACE(LPC_LISTEN_DEBUG, "Handle: %p\n", PortHandle);
-
-    /* Wait forever for a connection request */
-    for (;;)
-    {
-        /* Do the wait */
-        Status = NtReplyWaitReceivePort(PortHandle,
-                                        NULL,
-                                        NULL,
-                                        ConnectMessage);
-
-        _SEH2_TRY
-        {
-            /* Accept only LPC_CONNECTION_REQUEST requests */
-            if ((Status != STATUS_SUCCESS) ||
-                (LpcpGetMessageType(ConnectMessage) == LPC_CONNECTION_REQUEST))
-            {
-                /* Break out */
-                _SEH2_YIELD(break);
-            }
-        }
-        _SEH2_EXCEPT(EXCEPTION_EXECUTE_HANDLER)
-        {
-            Status = _SEH2_GetExceptionCode();
-            _SEH2_YIELD(break);
-        }
-        _SEH2_END;
-    }
-
-    /* Return status */
-    return Status;
+    UNIMPLEMENTED;
+    __debugbreak();
+    return STATUS_NOT_IMPLEMENTED;
 }
 
 /* EOF */
